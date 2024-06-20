c1d981b8d2265878b89c227a6e82e819e539a5ed
G384582023 <238458@st.takushoku-u.ac.jp>
2024-06-20 13:48:06 +0900

    jsのやつ

---
+++ 2_4layout.md_files/nav.js
@@ -0,0 +1,12 @@
+var files = [
+	{title:"講義資料（1/5）: CSS による見た目の設定", fname:"2_1css-intro.html"},
+	{title:"講義資料（2/5）: CSS の全体像", fname:"2_2css-structure.html"},
+	{title:"講義資料（3/5）: 演習", fname:"2_2_5css-ex.html"},
+	{title:"講義資料（4/5）: CSSの参考資料", fname:"2_6css-reference.html"},
+	{title:"講義資料（5/5）: CSSのデバッグ", fname:"2_5css-debug.html"},
+	{title:"課題", fname:"2_7task.html"},
+	{title:"参考資料: ボックスモデル", fname:"2_3box-model.html"},
+	{title:"参考資料: レイアウト", fname:"2_4layout.html"},
+];
+
+var themeTitle = "第2回CSSによるWebページの装飾";
